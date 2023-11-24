398f49e2658b5388587bf967e9be96e5efee0445
nghiaduc882005 <147359386+nghiaduc882005@users.noreply.github.com>
2023-11-24 08:04:48 +0700

    Add files via upload

--- btvn2-ss12.cpp
+++ btvn2-ss12.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
-int Printf(int a,int b){
-	printf("\n %d * %d = %d", a , b ,a * b);
-	if(a % b != 0){
+int Printf(int a,int b);
+	Printf("\n %d * %d = %d", a , b ,a * b);
+	if(a % b != 0) {
 		printf("\n %d khong chia het cho %d", a, b);
-	}else {
-		printf("\n %d / %d = %d", a, b, a / b);
+    }else {
+		printf("\n%d/%d=%d", a, b, a / b);
 	}
-}
 int main(){
 	int a,b,c;
 	printf("Nhap so lan luot a, b, c: ");
